80e2fbad66712add28ba28e3897fc957057ac55e
Shazammon <ammonorgill@gmail.com>
2022-12-15 14:21:00 -0700

    set up initial data input into arrays

--- solution.js
+++ solution.js
@@ -1,4 +1,11 @@
 let oneStack = ["B", "S", "J", "Z", "V", "D", "G"]
 let twoStack = ["P", "V", "G", "M", "S", "Z"]
 let threeStack = ["F", "Q", "T", "W", "S", "B", "L", "C"]
-let fourStack = ["Q", ]
+let fourStack = ["Q", "V", "R", "M", "W", "G", "J", "H"]
+let fiveStack = ["D", "M", "F", "N", "S", "L", "C"]
+let sixStack = ["D", "C", "G", "R"]
+let sevenStack = ["Q", "S", "D", "J", "R", "T", "G", "H"]
+let eightStack = ["V", "F", "P"]
+let nineStack = ["J", "T", "S", "R", "D"]
+
+f
